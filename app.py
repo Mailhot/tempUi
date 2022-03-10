33d29c8773ee7d17bab58a3d9dddc916ab1354f3
Mailhot <fmailhot@maxidrill.com>
2022-03-10 00:24:19 -0500

    added sucesfully the temp diff display

--- app.py
+++ app.py
@@ -17,9 +17,14 @@ filename = "log_" + str(time.time()) + ".csv"
 last_rows = []
 st.title('Smoking temperature logger.')
 placeholder = st.empty()
+grill_temp_diff = 0
+meat_temp_diff = 0
 
-# last_rows = np.random.randn(1, 1)
 data = pd.DataFrame([[20.0,20.0]], columns=['Grill', 'Meat1'])
+
+# Persistent session data
+if 'df' not in st.session_state:
+    st.session_state.df = data
 
 chart = st.line_chart(data)
 # table = st.dataframe(data)
@@ -42,10 +47,19 @@ while True:
         # print(device, temp_c)
     
 
-        with open("test_data.csv","a") as f:
+        with open(filename,"a") as f:
             writer = csv.writer(f, delimiter=",")
             writer.writerow([time.time(), address1, temp1, address2, temp2])
         new_rows = pd.DataFrame([[float(temp1), float(temp2)]], columns=['Grill', 'Meat1'])
+        st.session_state.df = st.session_state.df.append(new_rows, ignore_index=True)
+        if len(st.session_state.df.index) > 51:
+            # print(st.session_state.df.iloc[-50]['Grill'])
+            grill_temp_diff = float(temp1) - st.session_state.df.iloc[-50]['Grill']
+            meat_temp_diff = float(temp2) - st.session_state.df.iloc[-50]['Meat1']
+        elif len(st.session_state.df.index) > 6:
+            # print(st.session_state.df.iloc[-1]['Grill'])
+            grill_temp_diff = float(temp1) - st.session_state.df.iloc[-5]['Grill']
+            meat_temp_diff = float(temp2) - st.session_state.df.iloc[-5]['Meat1']
         chart.add_rows(new_rows)
         # table.add_rows(new_rows)
         # print(last_rows)
@@ -58,8 +72,9 @@ while True:
 
         col1, col2 = st.columns(2)
 
-        col1.metric(label="Grill Temp", value=str(temp1) + " °C", delta=grill_temp_diff + " °C")
-        col2.metric(label="Meat Temp", value=str(temp2) + " °C", delta=meat_temp_diff + " °C")
+
+        col1.metric(label="Grill Temp", value=str(temp1) + " °C", delta=str(grill_temp_diff) + " °C")
+        col2.metric(label="Meat Temp", value=str(temp2) + " °C", delta=str(meat_temp_diff) + " °C")
 
     # progress_bar.progress(i)
     # last_rows = new_rows
